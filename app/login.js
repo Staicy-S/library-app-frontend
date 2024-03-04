3689b086fe018656aad316076edd414267b82410
Staicy Stadtler <staicy@hotmail.de>
2024-03-04 15:21:27 +0100

    add exit button to login page

--- app/login.js
+++ app/login.js
@@ -1,12 +1,36 @@
 import { globalStyles } from "../styles/global";
+import { COLORS } from "../styles/constants";
 import { Text, TextInput, View, Button, Pressable } from "react-native";
 import { useState } from "react";
 import { router } from "expo-router";
 
 export default function Login() {
   const [name, setName] = useState("");
+  const showBackButton = router.canGoBack();
   return (
-    <View style={[globalStyles.container, { justifyContent: "center" }]}>
+    <View
+      style={[
+        globalStyles.container,
+        {
+          justifyContent: "center",
+          position: "relative",
+        },
+      ]}
+    >
+      {showBackButton && (
+        <Pressable
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+          }}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <Text style={{ backgroundColor: COLORS.dark, fontSize: 26 }}>❌</Text>
+        </Pressable>
+      )}
       <Text style={globalStyles.heading}>Login</Text>
       <View style={globalStyles.spacing}>
         <TextInput
