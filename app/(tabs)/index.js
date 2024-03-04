2a1901a84556c609a20c681466560b556a557312
Staicy Stadtler <staicy@hotmail.de>
2024-03-04 12:32:56 +0100

    initial commit, showing books

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
 import { globalStyles } from "../../styles/global";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const booksUrl = "http://192.168.0.158:3000/books";
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    async function loadBooks() {
+      try {
+        const { data } = await axios.get(booksUrl);
+        setBooks(data);
+        console.log(data);
+      } catch (error) {
+        // console.log(error);
+      } finally {
+        console.log("finish loading");
+      }
+    }
+    loadBooks();
+  }, []);
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.heading}>Homepage</Text>
-      <Text style={globalStyles.text}>Random Text</Text>
+      <Text style={globalStyles.heading}>📚 Library App</Text>
+      <FlatList
+        data={books}
+        keyExtractor={(book) => book.id}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <View>
+              <Text style={globalStyles.text}>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
